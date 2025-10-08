98c2a8849cd8807416b9391762981af17894b665
Goootaa <guilhermeduarte656@gmail.com>
2025-10-08 00:29:15 -0400

    if

---
+++ if.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int i;
+    printf("Digite um Numero:");
+    scanf("%d",&i);
+    if(i==0){
+        printf("gay.");
+    }
+    else{
+        printf("Ola amigo");
+    }
+}
